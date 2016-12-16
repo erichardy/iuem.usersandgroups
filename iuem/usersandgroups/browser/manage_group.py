8d05f1830fd08d8fb400fcd8d49e5f81507a6291
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-12-16 09:52:04 +0100

    install/uninstall group

---
+++ iuem/usersandgroups/browser/manage_group.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from zope.publisher.browser import BrowserView
+
+from plone import api
+
+from iuem.usersandgroups import utils
+
+# from iuem.usersandgroups import MessageFactory as _
+
+logger = logging.getLogger('iuem.usersandgroups:manageGroup')
+
+
+class manageGroup(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        # iuem_groups = getIuemGroups()
+        # utils.createGroupAndUsers('iuem')
+        # utils.deleteGroupAndUsers('iuem')
+        # utils.createGroupAndUsers('comiuem')
+        # utils.createGroupAndUsers('feiri')
+        # utils.createGroupAndUsers('ecofluxweb')
+        # utils.deleteGroupAndUsers('ecofluxweb')
+        # utils.deleteGroupAndUsers('feiri')
+        # utils.updateUsersPassword()
+        pass
+
+    def __call__(self, group, operation):
+        logger.info(group)
+        logger.info(operation)
+        if operation == 'install':
+            utils.createGroupAndUsers(group)
+            return group + ' installed'
+        if operation == 'uninstall':
+            utils.deleteGroupAndUsers(group)
+            return group + ' uninstalled'
+        return 'nothing to do'

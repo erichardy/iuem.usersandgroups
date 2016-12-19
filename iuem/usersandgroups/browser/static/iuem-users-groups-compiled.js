b47b785603a406573c8558dcf0ea0a6c65d2bb03
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-12-19 16:47:09 +0100

    resources

---
+++ iuem/usersandgroups/browser/static/iuem-users-groups-compiled.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+/* This is a bundle that uses RequireJS to pull in dependencies.
+   These dependencies are defined in the registry.xml file */
+
+
+/* do not include jquery multiple times */
+if (window.jQuery) {
+  define('jquery', [], function() {
+    return window.jQuery;
+  });
+}
+
+require([
+  'jquery',
+], function($, dep1, logger){
+  'use strict';
+
+  
+  // initialize only if we are in top frame
+  if (window.parent === window) {
+    $(document).ready(function() {
+      $('body').addClass('iuem-users-groups');
+      /* */
+    	// pre_captcha = $("#form a#autotoc-item-autotoc-5").html();
+  		// captcha = '<img src="++theme++plonetheme.tebl/images/thesis-form/code.png" /><br/>';
+  		// $("#form a#autotoc-item-autotoc-5").html(captcha + pre_captcha) ;
+  		// console.log('azeazeaze');
+	  /* */
+    });
+  }
+  
+
+});
+
+
+define("/Volumes/SSD/Dev/iuem.usersandgroups/iuem/usersandgroups/browser/static/iuem-usersandgroups.js", function(){});
+
